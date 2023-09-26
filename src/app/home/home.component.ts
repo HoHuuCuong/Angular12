76c17181c5886f680b527a84f0dc5cfaa2c73e31
Cuong <cuong312@gmail.com>
2023-09-26 17:33:22 +0700

    add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,13 +14,37 @@ export class HomeComponent implements OnInit {
       { ten:'Ổi', gia:13,haGia:0,pipe:1 },
       { ten:'Xoài', gia:-2,haGia:1,pipe:0 }
     ];
+    public districts:string[]=[];
+    public VietNamData=[
+      {
+        city:'An Giang',
+        district:[
+        "Thành phố Long Xuyên","Thành phố Châu Đốc","Thị xã Tân Châu","Huyện An Phú","Huyện Châu Phú","Huyện Châu Thành","Huyện Chợ Mới","Huyện Phú Tân","Huyện Thoại Sơn","Huyện Tịnh Biên","Huyện Tri Tôn"
+      ]
+      },
+      {
+        city:'Bà Rịa - Vũng Tàu',
+        district:[
+          "Thành phố Vũng Tàu","Thị xã Bà Rịa","Thị xã Phú Mỹ","Huyện Châu Đức","Huyện Côn Đảo","Huyện Đất Đỏ","Huyện Long Điền","Huyện Tân Thành","Huyện Xuyên Mộc"
+      ]
+      }
+    ];
   constructor() { }
 
   ngOnInit(): void {
-    console.log("trái cây = "+ this.traicay);
+    console.log("cities = ", this.VietNamData);
   }
  public resetName(): void{
   console.log("sadas");
   this.name="Cuong";
+ }
+ public changeCity(event:any){
+  const city=event.target.value;
+   console.log('event',city);
+  const search = this.VietNamData.filter((data)=> data.city===city);
+  if( search.length>0){
+    this.districts=search[0].district;
+  }
+
  }
 }
